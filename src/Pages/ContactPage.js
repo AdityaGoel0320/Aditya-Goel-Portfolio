7e8c06d945922d2a347edc6705e3b0fff4933443
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-13 00:07:36 +0530

    some edits

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ContactPage = () => {
-  return (
-    <div>ContactPage</div>
-  )
-}
 
-export default ContactPage
+
+
+    const [Email, setEmail] = useState("");
+
+    const [Content, setContent] = useState("");
+
+    const HandleChange = (e) => {
+      if (e.target.name === "Email") {
+        setEmail(e.target.value);
+      }
+
+      if (e.target.name === "Content") {
+        setContent(e.target.value);
+      }
+    }
+
+
+
+    return (
+      <>
+        <div>ContactPage</div>
+
+
+        <div id="Contact">
+          <div className="Contact_head">
+            <p>Connect with me</p>
+          </div>
+          <div className="Contact_intro">
+            <p>I'm always happy to connect and learn new things! You can reach out to me by filling out the form below</p>
+          </div>
+          <div className="Contact_form">
+            <form action="https://getform.io/f/b47c5986-f035-4696-8e26-78f6c1596f58" method="post">
+              <input type='email' name='Email' placeholder='Enter your Email' required value={Email} onChange={HandleChange} />
+
+              <input type='text' name='Content' placeholder='Topic of interest' required value={Content} onChange={HandleChange} />
+
+
+              <button type='submit' >Send</button>
+
+            </form>
+
+          </div>
+        </div>
+
+      </>
+    )
+  }
+
+  export default ContactPage ; 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,22 +2,29 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <div>
+        <>
 
-            <h1>
-                Hi I'm Aditya Goel
-            </h1>
-            <h2>
+            <div>
 
-                Front-end Web Developer
-            </h2>
+                <h1>
+                    Hi I'm Aditya Goel
+                </h1>
+                <h2>
 
-            <h3>
+                    Front-end Web Developer
+                </h2>
 
-                I am Aditya Goel and I am pursuing B.Tech in Information Technology from Maharaja Agrasen Institute Of Technology, Delhi. My specialty is frontEnd web development. Currently, I am learning MERN Stack development also i am coder in JAVA. As a passionate learner and a strong believer in gaining new skills and experiences through project work, I am eager to learn new technologies. I am interested in collaborating and exploring new opportunitie
-            </h3>
-        </div>
+                <h3>
+
+                    I am Aditya Goel and I am pursuing B.Tech in Information Technology from Maharaja Agrasen Institute Of Technology, Delhi. My specialty is frontEnd web development. Currently, I am learning MERN Stack development also i am coder in JAVA. As a passionate learner and a strong believer in gaining new skills and experiences through project work, I am eager to learn new technologies. I am interested in collaborating and exploring new opportunitie
+                </h3>
+            </div>
+            <div>
+<img src="./Images/avatar.jpg" alt="" />
+            </div>
+        </>
     )
+
 }
 
 export default HomePage
