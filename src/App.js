85f060898d2eabe71952a9ee1c030a76f8c7b141
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-12 13:05:52 +0530

    Aditya-Goel-Portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+    <>
+    <h1 className='flex justify-center items-center text-5xl'>Hello</h1>
+    </>
+  );
+}
+
+export default App;
