d4f414972eba805229ed490d2b988124299c88b3
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-12 14:01:15 +0530

    building ui and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ProjectsPage from './components/ProjectsPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
   return (
    
     <>
-    <h1 className='flex justify-center items-center text-5xl'>Hello</h1>
-    <div className="h-24 w-24 bg-red-500"></div>
+    <Navbar/>
+    <HomePage/>
+    <AboutPage/>
+    <ProjectsPage/>
+    <ContactPage/>
+    
     </>
   );
 }
