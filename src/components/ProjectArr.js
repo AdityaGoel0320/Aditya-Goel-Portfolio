4b0b2266d5f8f790cec273109a29d6051f2c1ab5
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-15 01:43:13 +0530

    some updates

--- src/components/ProjectArr.js
+++ src/components/ProjectArr.js
@@ -1,12 +1,64 @@
-let ProjectArr = [
+const ProjectArr = [
     {
-       name :" ReactJs-Styled-Components-Website" ,
-       img :"./Images/avatar.jpg" , 
-       repo : "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website" , 
-       link :"https://react-js-styled-components-website.vercel.app/" , 
-       desc :"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium commodi accusantium sit animi impedit in, repudiandae, soluta consectetur unde nobis aperiam? Fugiat pariatur expedita voluptatum eveniet ea vero quae temporibus sequi earum. Quae, iusto veritatis!" , 
-       techUsed : ["Reactjs" , "Javascroipt" , "Html" , "Css" , "Styledcompinents"]  ,   
-    }
-]
+        name: "Dynamic Pizza Paradise: A React-Tailwind Delight !",
+        img: "./Images/pizza.png",
+        repo: "https://github.com/AdityaGoel0320/Pizza-Store-ReactJs",
+        link: "https://pizza-store-react-js.vercel.app/",
+        desc: "Developed a dynamic and interactive Pizza Shop web using React.js,Tailwind CSS, HTML, and CSS. Utilized React hooks(useState, useEffect)and JavaScript functions(filter(), map()) forefficient data handling and enhanced userexperience.",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS" , "TailWindCss"],
+    },
 
-export default ProjectArr ; 
+
+    {
+        name: "Task Manager Pro: Task Mastery with React Magic!",
+        img: "./Images/todo.png",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Todo-App",
+        link: "https://react-js-todo-app-one.vercel.app/",
+        desc: "Created a Task Manager application with React.js, Tailwind CSS, HTML, and CSS, featuring local storage for data storage Utilized React hooks for managing state and building a responsive user interface Nostalgia Arcade: Relive Classics in Modern Style!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS"],
+    },
+
+    {
+        name: "Nostalgia Arcade: Relive Classics in Modern Style!",
+        img: "./Images/avatar.jpg",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website",
+        link: "https://react-js-styled-components-website.vercel.app/",
+        desc: "Transform your passion for gaming into a mesmerising web experience with our Game Center app, where classic games meet modern JavaScript mastery. Engage users with dynamic gameplay, responsive design, and a touch of nostalgia.🎮🌟",
+        techUsed: [ "JavaScript", "HTML", "CSS", ],
+    },
+    {
+        name: "ReactJs-Styled-Components-Website",
+        img: "./Images/avatar.jpg",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website",
+        link: "https://react-js-styled-components-website.vercel.app/",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium commodi accusantium sit animi impedit in, repudiandae, soluta consectetur unde nobis aperiam? Fugiat pariatur expedita voluptatum eveniet ea vero quae temporibus sequi earum. Quae, iusto veritatis!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS", "Styled-Components"],
+    },
+    {
+        name: "ReactJs-Styled-Components-Website",
+        img: "./Images/avatar.jpg",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website",
+        link: "https://react-js-styled-components-website.vercel.app/",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium commodi accusantium sit animi impedit in, repudiandae, soluta consectetur unde nobis aperiam? Fugiat pariatur expedita voluptatum eveniet ea vero quae temporibus sequi earum. Quae, iusto veritatis!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS", "Styled-Components"],
+    },
+    {
+        name: "ReactJs-Styled-Components-Website",
+        img: "./Images/avatar.jpg",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website",
+        link: "https://react-js-styled-components-website.vercel.app/",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium commodi accusantium sit animi impedit in, repudiandae, soluta consectetur unde nobis aperiam? Fugiat pariatur expedita voluptatum eveniet ea vero quae temporibus sequi earum. Quae, iusto veritatis!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS", "Styled-Components"],
+    },
+    {
+        name: "ReactJs-Styled-Components-Website",
+        img: "./Images/avatar.jpg",
+        repo: "https://github.com/AdityaGoel0320/ReactJs-Styled-Components-Website",
+        link: "https://react-js-styled-components-website.vercel.app/",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium commodi accusantium sit animi impedit in, repudiandae, soluta consectetur unde nobis aperiam? Fugiat pariatur expedita voluptatum eveniet ea vero quae temporibus sequi earum. Quae, iusto veritatis!",
+        techUsed: ["ReactJS", "JavaScript", "HTML", "CSS", "Styled-Components"],
+    },
+
+];
+
+export default ProjectArr;
