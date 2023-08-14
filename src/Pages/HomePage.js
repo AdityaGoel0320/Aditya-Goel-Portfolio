79a6b59e4f5b4e253149450701cd9790a32de4c6
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-08-15 02:23:35 +0530

    some updates

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,53 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 const HomePage = () => {
     return (
         <>
-
-            <div className="home_box ">
-                {/* <div className='w-2/3'> */}
-                <div className=''>
-
+            <div className="home_box">
+                <div className="">
                     <h1>
-                        Hi I'm <span className="my_name"> Aditya Goel</span>
+                        Hi, I'm <span className="my_name">Aditya Goel</span>
                     </h1>
                     <h3>
-
                         Front-end Web Developer
                     </h3>
                     <br />
                     <h4>
-
-                        I am Aditya Goel and I am pursuing B.Tech in Information Technology from Maharaja Agrasen Institute Of Technology, Delhi. My specialty is frontEnd web development. Currently, I am learning MERN Stack development also i am coder in JAVA. As a passionate learner and a strong believer in gaining new skills and experiences through project work, I am eager to learn new technologies. I am interested in collaborating and exploring new opportunities
+                        I am Aditya Goel, and I am pursuing B.Tech in Information Technology from Maharaja Agrasen Institute Of Technology, Delhi. My specialty is front-end web development. Currently, I am learning MERN Stack development, and I am also a coder in JAVA. As a passionate learner and a strong believer in gaining new skills and experiences through project work, I am eager to learn new technologies. I am interested in collaborating and exploring new opportunities.
                     </h4>
 
-                    <div className='flex items-center justify-center  gap-5 m-5'>
+                    <div className='flex items-center justify-center gap-5 m-5'>
                         <a href="https://github.com/AdityaGoel0320" target="_blank" rel="noopener noreferrer" className='color-black'>
                             <i id='i' className="fa-brands fa-github"></i>
                         </a>
-                        <a href="https://www.linkedin.com/in/aditya-goel-286245239" target="_blank" rel="noopener noreferrer"
-                            className='color-black'>
+                        <a href="https://www.linkedin.com/in/aditya-goel-286245239" target="_blank" rel="noopener noreferrer" className='color-black'>
                             <i id="i" className="fa-brands fa-linkedin-in"></i>
                         </a>
-                        <a href="https://twitter.com/AdityaGoel0320?t=DWSov778bgHXMkyQxELrow&s=09" target="_blank"
-                            rel="noopener noreferrer" className='color-black'>
+                        <a href="https://twitter.com/AdityaGoel0320?t=DWSov778bgHXMkyQxELrow&s=09" target="_blank" rel="noopener noreferrer" className='color-black'>
                             <i id='i' className="fa-brands fa-x-twitter"></i>
                         </a>
-                        {/* <button>Resume</button> */}
                         <button className='resume_btn'>
-                            <a target='_blank' href="google.com"><span className='resume_span'>Resume</span>
+                            <a target='_blank' href="google.com">
+                                <span className='resume_span'>Resume</span>
                             </a>
                         </button>
                     </div>
                 </div>
-
-                {/* <div className='w-1/3'>
-                    <img className='avtar' src="./Images/avatar.jpg" alt="" />
-                </div> */}
-            </div >
+            </div>
         </>
-    )
+    );
+};
 
-}
-
-export default HomePage
+export default HomePage;
