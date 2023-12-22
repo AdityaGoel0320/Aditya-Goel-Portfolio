1794ce2204e5fab8c8c26a49cbfed39bd521b3a5
Aditya Goel <adityagoelgithub0320@gmail.com>
2023-12-23 01:33:24 +0530

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -13,7 +13,14 @@ const HomePage = () => {
                     </h3>
                     <br />
                     <h4>
-                        I am Aditya Goel, and I am pursuing B.Tech in Information Technology from Maharaja Agrasen Institute Of Technology, Delhi. My specialty is front-end web development. Currently, I am learning MERN Stack development, and I am also a coder in JAVA. As a passionate learner and a strong believer in gaining new skills and experiences through project work, I am eager to learn new technologies. I am interested in collaborating and exploring new opportunities.
+                        Aditya Goel, currently pursuing B.Tech in Information Technology at
+Maharaja Agrasen Institute of Technology, Delhi. Specializing in MERN 
+stack for full-stack web development and proficient in Java coding. 
+Possessing industry-level experience from a 3-month internship in a 
+startup, I contributed to the development of a web application. As a 
+fervent learner and advocate for acquiring new skills through project 
+work, I am enthusiastic about exploring and collaborating on innovative 
+opportunities in the field.
                     </h4>
 
                     <div className='flex items-center justify-center gap-5 m-5'>
